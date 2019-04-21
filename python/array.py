9346c31231ebed2c470e8e882733569d1c9ccd92
yangdawei <yangdawei.hit@gmail.com>
2019-04-21 14:28:19 +0800

    python

--- python/array.py
+++
@@ -1,6 +0,0 @@
-
-import pandas as pd
-
-bspath = os.path.abspath('.')
-print(abspath)
-my_data = pd.read_csv("my_file.csv", sep=',',header=None)
--- python/pandas-test.py
+++
@@ -1,3 +0,0 @@
-
-id = ['s1', 's10', 's2', 's3']
-id.sort(key=lambda x: int(x[1:]))
